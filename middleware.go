5189527025f09755da8addc62362998f4228ae3c
Tim Marshall <tim@codingbeard.com>
2020-02-14 08:47:52 +0000

    Remove webshield state usage and replace with interface

--- middleware.go
+++ middleware.go
@@ -1,11 +1,34 @@
 package cbweb
 
 import (
+	"bytes"
 	"github.com/valyala/fasthttp"
-	"webshield/state"
+	"log"
+	"runtime"
+	"strings"
 )
 
+
+type ErrorHandler interface {
+	Error(e error)
+}
+
+type DefaultErrorHandler struct{}
+
+func (d DefaultErrorHandler) Error(e error) {
+	buf := make([]byte, 1000000)
+	runtime.Stack(buf, false)
+	buf = bytes.Trim(buf, "\x00")
+	stack := string(buf)
+	stackParts := strings.Split(stack, "\n")
+	newStackParts := []string{stackParts[0]}
+	newStackParts = append(newStackParts, stackParts[3:]...)
+	stack = strings.Join(newStackParts, "\n")
+	log.Println("ERROR", e.Error()+"\n"+stack)
+}
+
 type MiddlewareHandler struct {
+	ErrorHandler ErrorHandler
 	middleware []func(ctx *fasthttp.RequestCtx) (bool, error)
 	final      func(ctx *fasthttp.RequestCtx)
 }
@@ -26,7 +49,9 @@ func (m MiddlewareHandler) Handle(ctx *fasthttp.RequestCtx) {
 	for _, middleware := range m.middleware {
 		ok, e := middleware(ctx)
 		if e != nil {
-			state.Errors.Error(e)
+			if m.ErrorHandler != nil {
+				m.ErrorHandler.Error(e)
+			}
 		}
 		if !ok {
 			return
